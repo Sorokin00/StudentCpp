70d14cd6b34e20daaa7b789305f5a7ff9fd5aacc
Sorokin00 <44114062+Sorokin00@users.noreply.github.com>
2018-10-31 12:45:08 +0500

    Create 4.cpp

---
+++ lab01/Ур0/4.cpp
@@ -0,0 +1,44 @@
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char m = 'Y';
+	while (m == 'Y')
+	{
+		int a;
+		int b;
+		cout << "\nДобрый день! Введите два числа\n";
+		cin >> a;
+		cin >> b;
+		cout << "Вы ввели числа" << a << " и ";
+		cout << b << "\n";
+		cout << "Что вы хотите делать?\n1.+\n2.-\n3.*\n4./\n";
+		int t;
+		cin >> t;
+		cout << "Вы выбрали:" << t << "\n";
+		int c;
+		if (t == 1)
+		{
+			c = a + b;
+		}
+		if (t == 2)
+		{
+			c = a - b;
+		}
+		if (t == 3)
+		{
+			c = a * b;
+		}
+		if (t == 4)
+		{
+			c = a / b;
+		}
+		cout << "Результат:" << c;
+		cout << "\nХотите повторить? Y/N";
+		cin >> m;
+	}
+}
