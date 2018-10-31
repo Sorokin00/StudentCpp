56769bd0605246d4b1196fc28b99ab5c5e522355
Sorokin00 <44114062+Sorokin00@users.noreply.github.com>
2018-10-31 12:22:27 +0500

    Update 2.cpp

--- lab01/Ур0/2.cpp
+++ lab01/Ур0/2.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main()
 {
-	cout << "Write your name\n";
 	char a;
-	cout << "How old are you?\n";
 	int b;
+	cout << "Write your name\n";
+	cin >> a;
+	cout << "How old are you?\n";
+	cin >> b;
 	cout << "Hello, "<<a<<". You're "<<b<<" years old.";
 }
