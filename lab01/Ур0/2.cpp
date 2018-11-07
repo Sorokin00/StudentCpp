496f74d57c750662291371f3359cd89db29d293e
Sorokin00 <44114062+Sorokin00@users.noreply.github.com>
2018-11-07 17:14:55 +0500

    Update 2.cpp

--- lab01/Ур0/2.cpp
+++ lab01/Ур0/2.cpp
@@ -4,10 +4,16 @@ using namespace std;
 int main()
 {
 	char a;
-	int b;
 	cout << "Write your name\n";
 	cin >> a;
-	cout << "How old are you?\n";
+	cout << "Write year, when you was born\n";
+	int b;
 	cin >> b;
-	cout << "Hello, "<<a<<". You're "<<b<<" years old.";
+	cout << "Write month, when you was born\n";
+	int c;
+	cin >> c;
+	cout << "Write day, when you was born\n";
+	int d;
+	cin >> d;
+	cout << "Hello, "<<a<<". You're "<<b<<"/"<<c<<"/"<<d<<" years old.";
 }
